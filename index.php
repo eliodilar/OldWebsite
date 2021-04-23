125fefb1d7f7a2201e7b6afc11000eac4ac09005
eliodilar <44893881+eliodilar@users.noreply.github.com>
2021-04-23 22:23:27 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html >
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" >
+        <link rel="stylesheet" href="top.css" />
+	
+        <title>Jerome Chapoul - CV</title>
+    </head>
+
+	<body id="body">
+		
+	<audio autoplay loop > 
+		<source src="musiques/automatica.ogg" type="audio/ogg">
+		<source src="musiques/automatica.mp3" type="audio/mpeg">
+		<source src="musiques/automatica.flac">
+    </audio>
+				<a id="section" href="http://www.jerome-chapoul.com/CVbegin.php" ><img src="images/ttti.jpg" alt="#" /></a>
+		
+				<a id="section" href="http://www.jerome-chapoul.com/bfs.php" target="blank" ><img src="images/bfs2.png" alt="#" /></a>
+
+				<a id="section" href="https://github.com/eliodilar" target="blank" ><img src="images/github.png" alt="#" /></a>
+
+				<a id="section" href="https://eliodilar.artstation.com/" target="blank" ><img src="images/Portfolio.png" alt="#" /></a>
+		
+				<a id="section" href="http://www.jerome-chapoul.com/TTP.php" target="blank" ><img src="images/ttp.png" alt="#" /></a>
+
+	</body>	
+	
+	<audio autoplay loop controler > 
+			<source src="musiques/SFSCIFI.ogg" type="audio/ogg">
+			<source src="musiques/SFSCIFI.mp3" type="audio/mpeg">
+    </audio>
+
+
+</html>
+
+	    
