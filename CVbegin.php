36880a98adce3935d44dcad0a0268a927b14ea6d
eliodilar <44893881+eliodilar@users.noreply.github.com>
2021-04-23 15:13:45 +0200

    Create CVbegin.php

---
+++ CVbegin.php
@@ -0,0 +1,240 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width" >
+        <link rel="stylesheet" href="cv.css" />
+	
+        <title>Jerome Chapoul - CV</title>
+    </head>
+    
+    <body id="body">
+        	<header id="head">
+        		<h1 class="titre"> Développeur logiciel et robotique.</h1>
+                <nav>
+<ul id="menu-demo2">
+	<li><a href="#">Candidature</a>
+						
+		<ul>
+			<div class="ongletd">
+			<li><a  href="newCV2020afjv.pdf" target="_blank" title="Cv.">CV</a></li>
+			<li><a  href="lmChapoulWarpFrog.pdf" target="_blank" title="Lettre de motivation.">Motivations</a></li>
+			</div>
+		</ul>
+	</li>
+	<li><a href="#">Concours CNRS</a>
+		<ul>
+			<div class="ongletd">
+			<li><a  href="projet/cnrs.pdf" target="_blank" title="Synthèse de deux ans d'activité au CNRS à l'Institut de Physique Nucléaire d'Orsay."> CNRS 2015</a></li>
+                        <li><a  href="projet/Concours2018.pdf" target="_blank" title="Mon expérience en tant que développeur logiciel et informatique scientifique."> CNRS 2018</a></li>
+			</div>
+ 		</ul>
+	</li>
+	<li><a href="#">Musique</a>
+		<ul>
+			<div class="ongletd">
+			<li><a href="music.php" target="_blank" title="Mes compositions personnel.">Projet solo</a></li>
+			<li><a  href="grp.php" target="_blank" title="Par içi le coin musique.">Musique pro</a></li>
+			</div>
+		</ul>
+	</li>
+	<li><a href="#">FTTCM studio</a>
+		<ul>
+			<div class="ongletd">
+			<li><a href="FTTCM.php" target="_blank" title="Ma voie.">Modélisation 3D</a></li>
+			<li><a href="gamming.php" target="_blank" title="Mon exutoire.">Vidéos Gaming</a></li>
+			</div>
+		</ul>
+	</li>
+	<li><a href="#">Me contacter</a>
+		<ul>
+			<div class="ongletd">
+			<li><a href="contact.php" target="_blank" title="Vous souhaitez me contacter? ">Contact</a></li>
+			</div>
+		</ul>
+	</li>
+</ul>    
+    			
+                </nav>
+        	</header>
+
+        	<section>
+                <div id="struct1">
+
+                    <div id="compinf">
+                    <p class="subtitle"><u><strong>Diplômes:</strong></u></p>
+                         <ul class="textes">
+                           <li> <p>2011 Licence Pro Automation et Robotique.</p></li>
+                            <li><p>2009 Bts Maintenance Industrielle.</p></li>
+                           <li> <p>2006 Bac Pro Electrotechnique.</p></li>
+                            <li><p>2004 Cap/Bep Electrotechnique.</p></li>
+                          </ul>
+
+                     
+                     <p class="subtitle"><u><strong>Formations:</strong></u></p>
+                         <ul class="textes">
+							<li><p>Unreal Engine 4 (WebSchool)2021.</p></li>
+							<li><p>Blender (Notions) 2020.</p></li>
+							<li><p>Unity 3D (Notions) 2020.</p></li>
+							<li><p>Love 2D ,LUA (WebSchool) 2020.</p></li>
+							<li><p>R.Mecklenburg GNU Make (gcc/g++/gcv) 2019.</p></li>
+							<li><p>B.Chapman Using OpenMp 2019.</p></li>
+							<li><p>Shm.h XSI Shared Memory 2018</p></li>
+							<li><p>E.Gamma Design patterns:Reusable object 2018</p></li>	
+							<li><p>R.Miles UML2.0 2018</p></li>	
+							<li><p>M.Seveaux Algorithme et Graph 2018</p></li>
+							<li><p>A.Tanenbaum Architecture des ordinateurs 2018</p></li>	
+                            <li><p>Python3, Vpython, Xml_Parser, Plot... 2018.</p></li>
+                            <li><p>FunMooc Peser l'Univers Obs-Meudon, 2018.</p></li>
+			    			<li><p>Certificat HTML5 et CSS3 OpenClassRooms 2016.</p></li>
+                            <li><p>Certificat P.O.O C# 2016.</p></li>
+							<li><p>Auto formation SGBDR-SQL - 2015.</p></li>
+                            <li><p>C89 CNRS-IDRIS, 2015.</p></li>
+                            <li><p>LabVIEW 2015.</p></li>
+                            <li><p>Auto formation Visual Basic Script - 2014.</p></li>
+                            <li><p>Maintenance et programmation robot Abb/Fanuc.</p></li>
+                        </ul>
+
+                    <p class="subtitle"><u><strong>Languages:</strong></u></p>
+                    <ul class="textes">
+                            <li><p>Français, langue maternel.</p></li>
+                            <li><p>Anglais Lu, écris, parlé niveau opérationnel.</p></li>
+                         </ul>
+                    </div>
+
+
+
+              <div id="compinf">
+				  <p class="subtitle"><u><strong>Spéçialisation Unreal Engine:</strong></u></p>
+                    <ul class="textes">
+                            <li><u><strong>Back-end Programming:</strong></u></li>
+                            <p>Blueprint and C++</p>
+							<li><u><strong>Front-end programming:</strong></u></li>
+                            <p>Widget</p>
+                            <li><u><strong>Network:</strong></u></li>
+                            <p>Pixel streaming, dedicated serveur, local serveur</p>
+                            <li><u><strong>Artificial intelligence</strong></u></li>
+                            <p>Pawn sensing, Béhavior tree</p>
+							<li><u><strong>Shaders:</strong></u></li>
+                            <p>Post-process and materials ( bases)</p>
+							<li><u><strong>FX:</strong></u></li>
+                            <p>Niagara( notions )</p>
+                        </ul>
+				  
+                    <p class="subtitle"><u><strong>Informatique logiciel:</strong></u></p>
+                    <ul class="textes">
+                            <li><u><strong>Langages:</strong></u></li>
+                            <p> Blueprint, C/C++/C#, Python3, Lua, Batch(notions), SQL, VBS, XML/HTML5/CSS3, PhP/Java/Assembleur GAS(notions), <a class="liensection" href="code.php" target="_blank" title="Ma manière de coder">Coding 
+							style.</a></p>
+							<li><u><strong>Graphique:</strong></u></li>
+                            <p>Unity 3D, Blender, Vpython, Mg-Engine, Love 2D.</p>
+                            <li><u><strong>Spéçialisation:</strong></u></li>
+                            <p>C/C++, Programmation orienté objet, Multi-threading (OpenMP, Pthread.h, POSIX OS), Inter-process DDS(shm.h), simulation, Game/LevelDesign.</p>
+                            <li><u><strong>IDE-CVS:</strong></u></li>
+                            <p>Visual studio code, Zéro brain studio, SniperSim, Eclipse, Arduino, GIThub, Oracle/PhpMyAdmin (notions).</p>
+							<li><u><strong>Compilation:</strong></u></li>
+                            <p>GNU/GCC-G++, Make, Cmake(notions),Waf(notions)</p>
+                            <li><u><strong>OS:</strong></u></li>
+                            <p>Windows, Unix Like, Linux Ubuntu 14/16/18.04 - Backtrack R5(Notions)</p>
+                            <li><u><strong>Micro Contrôleur-robotique:</strong></u></li>
+                            <p>Pepeer, <a class="liensection" href="images/ABB.png" target="_blank" title="IRB 6600">ABB</a>, Arduino, Raspberry  
+						 	pi (notions), Mbed(notions)</p>
+                        </ul>
+
+                    <p class="subtitle"><u><strong>Informatique instrumentale:</strong></u></p>
+                    <ul class="textes">
+                            <li><u><strong>Instrumentation IDE:</strong></u></li>
+                            <p>LabView, RTMaps.</p>
+                            <li><u><strong>HardWare Instrumentation:</strong></u></li>
+                            <p>Agilent, Keithley, CABTF2 Cernox, Radioprotection(Berthold), Cryogénie (notion)  ...</p>
+                  </ul>
+
+                  <p class="subtitle"><u><strong>Informatique industriel:</strong></u></p>
+                    <ul class="textes">
+                       <li><u><strong>Automatisme:</strong></u></li>
+                            <p>Ladder, List(notions), SCADDA, SCPI, SHT.</p>
+                            <li><u><strong>IDE:</strong></u></li>
+                            <p>Step7, WinCC, TIA portal, Logo's, Cls3, Schneider twido, OPC-serveur(Siemens).</p>
+                   </ul>
+
+                 <p class="subtitle"><u><strong>Compétences CAO:</strong></u></p>
+                    <ul class="textes">
+                        <li><u><strong>CAO:</strong></u></li>
+                            <p>SolidWorks(notions), Visio.</p>
+                   </ul>
+              </div>
+					
+					
+		 <div id="compindus">
+                    <p class="subtitle"><u><strong>Théorie:</strong></u></p>
+                    <ul class="textes">
+                                <li><p>Électrotechnique: R&D appliqué commande/puissance basse tension, traitement du signal.</p></li>
+                                <li><p>Mécanique: RDM, usinage manuel, soudure(notions(tig/mig)), ajustements(notions).</p></li>
+                                <li><p>Fluides industriel: R&D appliqué Pneumatique/hydraulique.</p></li>
+                                <li><p>Maintenance: méthode maintenance: 5S/Pareto/loi de poisson/Wheibull/indicateurs/GMAO....</p></li>
+                                <li><p>Nucléaire: physique des particules/ondes.</p></li>
+                                <li><p>Astrophysique: Kepler(notions), Gravitation universel, relativités, cosmologie(notions).</p></li>
+								<li><p>Physique fondamentale: Diagram de Feynman, Maxwell's, cryogénie, thermodynamique, semi/supra conducteur, théorie des bandes... </p></li>
+								<li><p>Mathématiques appliqués: Analyse(en cours), Calcul infinitésimale (notions), équations différentielles, Nyquist/bodes(Notions), calcul matricielles(notions), 
+								linéarisation de fonctions...</p></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div id="struct1">
+                    <div id="experiences">
+                    <p class="subtitle"><u><strong>Expériences:</u></strong></p>
+                    <ul class="textes">
+						<li><p>07/2020 au 01/2021 Unreal Engine Développeur Freelance - Lançement développement et démarchage pour le développement du jeu de science fiction DeepBlue. Réalisation pour un entrepreneur, d'un POC pour la levée de fond d'une application jouable à la troisième personne, perméttant l'intéraction entre spectateur, intélligence artificiel et artiste/sportif, pour andiguer l'isolement sociale lié à l'épidémie de Covid-19, via un environement virtuel multi utilisateur online.   <a  class="liensection" href="http://www.jerome-chapoul.com/bfs.php" target="_blank" title=" Projet NOOMAN ">Brain Fantasy Studio </a>. Développement sous Unreal Engine, En remote agile.</p></li>
+							<li><p>10/2018 au 10/2019 Ingénieur développement logiciel <a  class="liensection" href="https://www.labsticc.fr/en/news/953-nooman-notifying-memories-for-dynamic-data-flow-application.htm" target="_blank" title=" Projet NOOMAN ">Projet NOOMAN </a> . Prise en main du simulateur de réseau sur puce architecture Von_Neumann <a  class="liensection" href="https://snipersim.org/w/The_Sniper_Multi-Core_Simulator" target="_blank" title=" SniperSim "> SniperSim </a>, pour le portage de codes  générés de type dataflow statique et dynamique sur des SOC/NOC à jeux d’instruction CISC intel X86_64.	                                     L’objectif était d’étudier des modèles ‘’éxotiques’’ d’architectures Von-Neumann simulés  pour  intégrer un nouveau composant et diminuer les érreurs d’accès caches mobilisant ~60% des ressources énergétiques journalière pour le rapatriement des données entre la RAM jusqu’aux cache de niveau 1 dans les différents niveaux de cache, le but d’intégrer un nouveau composant sur les cartes mères permettant de diminuer la charge de travail processeur du à ces requètes permanentes, successive et inutile, en définissant la mémoire vivre maitre sur le SOC et non le processeur, libérant ainsi la charge de travail de ce dernier.
+J’ai porté la compilation GNU de ce simulateur sur Ubuntu 18,04, ais participé à l’encapsulation dans un docker avec un accès ssh et ai déverminé certaines étapes de la gestion des affinités du SMT Posix dans les générateurs de codes dataflow ( ORCC, Preesm ) . 
+De mème, j’ai remonté des erreurs d’interprétation de micro-instructions assembleur (TZCNT – extension BMI1) dans le simulateur sniper-sim lors de la génération de fichiers de traces durant l’exécution de codes générés. </p></li>
+							
+						<li><p>09/2016 au 12/2017   Ingénieur développement logiciel à Ifsttar Satory - Développement et intégration d'une simulation réaliste de communication par cannaux de propagations wifi, peer to peer, temps réel, et réalisation du ProGiciel 
+							
+							Pour l’IFSTTAR l’ai travaillé sur l’intégration et le développement d’une couche de communication peer to peer wifi temps réel pour la simulation de communications entre véhicules autonomes dans le logiciel ADAS nommé <a  class="liensection" href="https://www.esi-group.com/fr/solutions-logicielles/environnement-virtuel/systemes-controles-virtuels/pro-sivictm" target="_blank" title=" ProSivic "> ProSivic </a> tournant simultanément sous OS Unix-like et Windows, intégré sur un <a class="liensection" href="images/bdijpg.jpg" target="_blank" title="Visuel simulateur"> simulateur</a> de pilotage develter dynamique et mécanique munit d’un casque VR SDK2 Oculus. 		
+J’ai développé et intégré un SMT temps réel gérant l’acquisition physique de données et de capteurs, dans une mémoire partagé type DDS, par l’intermédiaire d’une .DLL, j’ai ensuite couplé ces données au moteur graphique MG-Engine du logiciel Pro-sivic par l’intermédiaire de canaux de propagations créés par le simulateur wifi temps réel NS3, pour permettre la récupération de données de vitesses, positions… entre les objets véhicules instanciés.								     
+J’ai ensuite utilisé différents patterns comme le singleton pour créer un routage dynamique des données en cas de perte d’émétteurs.
+Dynamisation Informatique/mécanique d'un type Develter, Réalité virtuel, suivi de chantier.<a class="liensection" href="images/ifsttar.jpg" href="images/tu.jpg" target="_blank" title="Visuel soft"> visuel du projet réalisé.</a> - <a class="liensection" href="images/tu.png" target="_blank" title="Shell visuel">exemple de tests unitaires.</a> Publication scientifique:<a class="liensection" href="article/papi.pdf" target="_blank" title="Pro-sivic publication.">Paper.</a></p></li> 
+							<li><p>06/2016 au 07/2016  Automaticien pour Socofer à joué les Tours. Spécification fonctionnel et logiciel pour le développement d'une draisine bi-directionnel.</p></li>
+						
+                            <li><p>06/2013 au 09/2015  Ingénieur d'étude Contrôle et commande en instrumentation Projets Européens MYRRAH/MAX, SPIRAL2, ESS(RRR)à l'Institut de Physique Nucléaire d'Orsay. Etude et développement du <a class="liensection" href="projet/secu106.pdf" target="_blank" title="Description du système de radioprotection.">système de radioprotection.</a> hall expérimentale Supratech, Retrofit automatisme et contrôle commande du <a class="liensection" href="projet/MYRRHA.pdf" target="_blank" title="L'enjeux du projet MYRRHA.">projet MYRRHA</a>, étude et édéveloppement informatique/électrotechnique <a class="liensection" href="projet/RRR.pdf" target="_blank" title="Description du projet RRR.">Expérimentation residual ratio resistivity,</a> intégration des modules d'acquisitions cryogénique<a class="liensection" href="projet/cabtf.pdf" target="_blank" title="Intégration du cabtf2.">CABTF2</a>.</p></li>
+                            <li><p>01/2012 au 06/2013 Formateur en robotique (National) Pour Avenir formation à Angers. Formation de groupe de 2 à 10 personnes sur l'exploitation, <a class="liensection" href="projet/maintabb.pdf" target="_blank" title="Etalonnage robot ABB irb1440.">la maintenance.</a> et <a class="liensection" href="projet/progabb.pdf" target="_blank" title="Programmation standard des robots ABB.">la programmation.</a></p></li>
+                            <li><p>03/2010 au 11/2011 Apprenti Licence pro intégrateur/laseriste pour Themis technologies à Ambillou. Montage mécanique, étude électrotechnique et automatisme.<a class="liensection" href="projet/l3.pdf" target="_blank" title="Rapport de L3">Rapport de licence pro disponible içi.</a></p></li>
+                            <li><p>09/2007 au 09/2009 Apprenti Bts Maintenance Industrielle pour l'entreprise FOC à Avoine. Bureau d'étude mécanique, méthode maintenance, travaux neufs, maintenance électro-mécanique.</p></li>
+                            <li><p>07/2007 au 08/2007 Opérateur sur machine de production pour l'entreprise FOC à Avoine.</p></li>
+                            <li><p>05/2007 au /06/2007 Câbleur monteur industriel pour l'entreprise Forclum à Joué les tours.</p></li>
+                            <li><p>12/2006 au 04/2007 Technicien de maintenance industrielle et travaux neufs pour Wezer à Mazières.</p></li>
+                            <li><p>08/2006 au 09/2006 Câbleur monteur Electromécanique pour Saft Power System à Joué les Tours.</p></li>
+                            <li><p>06/2006 au 09/2006 Manoeuvre station d'épuration autoroute pour Sogéa à Saint Avertin.</p></li>
+                        </ul>
+                    </div>
+
+                    <div id="picsind">
+                    <!--<img src="images/ddd.jpg" alt="animation" />-->
+                    <video autoplay loop width="520" height="300" muted>
+                    <source src="video/mars.webm">
+                    <source src="video/mars.mp4">
+                    <source src="video/mars.ogg">
+                    <source src="video/mars.ts">
+                    </video>
+                       
+                    </div>
+                </div>
+        	</section>
+			
+			<footer id="footer">
+                <div class="subtitle">Mes activités:</div> <p> <span id="info">Voile croisière</span> - <span id="info">Astronomie/Astrophysique</span> - <span id="Music">Musique.</span> - <span id="cine">Cinéma</span> - <span id="info">Informatique</span>.</p>
+			</footer>
+		
+    </body>
+	
+    <audio autoplay loop controler> 
+		<source src="musiques/york.ogg" type="audio/ogg">
+		<source src="musiques/york.mp3" type="audio/mpeg">
+		<source src="musiques/york.flac">
+    </audio>
+	
+
+</html>
